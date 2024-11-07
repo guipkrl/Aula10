d132f1b460348bef7724235559fa330000bf01f9
guipkrl <guilherme_abelio@hotmail.com>
2024-11-07 12:31:41 -0300

    Aula 07/11
    Aula 07/11

---
+++ servidor/index.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const cors = require ('cors');
+const mysql = require ('mysql2');
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'mysqlfatec',
+    database: 'aulabd'
+});
+
+//Conectando ao banco de dados
+db.connect((erro)=>{
+    if (erro){
+        console.error("Erro ao conectar ao MySQL:", erro);
+    } else {
+        console.log("Conectado ao MySQL com sucesso");
+    }
+});
